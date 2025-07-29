ca82221c2c6d3897183848db9d5a33d00908a018
RinLaboratory <matiaschiongcampos@gmail.com>
2025-07-28 22:27:32 -0400

    refactor: updated layout

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,11 +1,16 @@
 import { AccountContextProvider } from "~/components/account-context/account-context";
 import NavBar from "~/components/navbar/navbar";
 import { getAccountInfo } from "./actions";
+import { redirect } from "next/navigation";
 
 export default async function AuthenticatedLayout(props: {
   children: React.ReactNode;
 }) {
   const currentUser = await getAccountInfo();
+
+  if (!currentUser) {
+    redirect("/login");
+  }
 
   return (
     <AccountContextProvider user={currentUser}>
