2f657252099dbeb34aa38343d353fab6cb3d806b
RinLaboratory <matiaschiongcampos@gmail.com>
2025-07-30 00:59:13 -0400

    refactor: removed legacyBehavior tag

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function Custom404() {
     <div>
       <h1>404 - Página no encontrada</h1>
       <p>La página que estás buscando no existe.</p>
-      <Link href="/" legacyBehavior>
+      <Link href="/">
         <a>Volver a la página de inicio</a>
       </Link>
     </div>
